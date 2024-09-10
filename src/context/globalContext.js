740dd9f6d12d9a822b852255d501e8f668810b5b
mayowa <matthewoluwatotin2020@gmail.com>
2024-09-10 22:59:53 +1245

    change server

--- src/context/globalContext.js
+++ src/context/globalContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react"
 import axios from 'axios'
 
 
-const BASE_URL = "https://expenses-tracker-4bfl.onrender.com/api/v1/";
+const BASE_URL = "https://expenses-tracker-umber.vercel.app/api/v1/";
 
 
 const GlobalContext = React.createContext()
